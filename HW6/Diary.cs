af727dca2ae4a164c3300705ed04a08513e00e36
NikulinIlya <ilia.nikulin@gmail.com>
2019-05-29 16:35:24 +0300

    make Diary, Note structures with parent references

---
+++ HW6/Diary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_6
+{
+    /// <summary>
+    /// Структура "Ежедневник"
+    /// </summary>
+    struct Diary
+    {
+        /// <summary>
+        /// Создание объекта ежедневника
+        /// </summary>
+        /// <param name="DiaryName">Название ежедневника</param>
+        /// <param name="DirectoryPath">Путь к ежедневнику</param>
+        /// <param name="CreatedAt">Время создания</param>
+        /// <param name="UpdatedAt">Время последнего редактирования</param>
+        public Diary(string DiaryName, string DirectoryPath, DateTime CreatedAt, DateTime UpdatedAt)
+        {
+            this.diaryName = DiaryName;
+            this.path = DirectoryPath;
+            this.createdAt = CreatedAt;
+            this.updatedAt = UpdatedAt;
+        }
+
+        #region Свойства
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string DiaryName { get { return this.diaryName; } set { this.diaryName = value; } }
+
+        /// <summary>
+        /// Путь
+        /// </summary>
+        public string DirectoryPath { get { return this.path; } set { this.path = value; } }
+
+        /// <summary>
+        /// Время создания
+        /// </summary>
+        public DateTime CreatedAt { get { return this.createdAt; } set { this.createdAt = value; } }
+
+        /// <summary>
+        /// Время последнего редактирования
+        /// </summary>
+        public DateTime UpdatedAt { get { return this.updatedAt; } set { this.updatedAt = value; } }
+
+        #endregion
+
+        #region Поля
+
+        /// <summary>
+        /// Поле "Название"
+        /// </summary>
+        private string diaryName;
+
+        
+        /// <summary>
+        /// Поле "Путь к файлу"
+        /// </summary>
+        private string path;
+
+        /// <summary>
+        /// Поле "Время создания"
+        /// </summary>
+        private DateTime createdAt;
+
+        /// <summary>
+        /// Поле "Время последнего редактирования"
+        /// </summary>
+        private DateTime updatedAt;
+
+        #endregion
+    }
+}
---
+++ HW6/Note.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_6
+{
+    /// <summary>
+    /// Структура "Запись в ежедневник"
+    /// </summary>
+    struct Note
+    {
+        /// <summary>
+        /// Создание объекта записи
+        /// </summary>
+        /// <param name="NoteName">Название заметки</param>
+        /// <param name="NoteText">Текст заметки</param>
+        /// <param name="DiaryName">Название ежедневника</param>
+        /// <param name="CreatedAt">Время создания</param>
+        /// <param name="UpdatedAt">Время последнего редактирования</param>
+        public Note(string NoteName, string NoteText, string DiaryName, DateTime CreatedAt, DateTime UpdatedAt)
+        {
+            this.noteName = NoteName;
+            this.noteText = NoteText;
+            this.diaryName = DiaryName;
+            this.createdAt = CreatedAt;
+            //this.createdAt = DateTime.Now;
+            //this.updatedAt = DateTime.Now;
+            this.updatedAt = UpdatedAt;
+        }
+
+        #region Свойства
+
+        /// <summary>
+        /// Название заметки
+        /// </summary>
+        public string NoteName { get { return this.noteName; } set { this.noteName = value; } }
+
+        /// <summary>
+        /// Текст заметки
+        /// </summary>
+        public string NoteText { get { return this.noteText; } set { this.noteText = value; } }
+
+        /// <summary>
+        /// Название ежедневника
+        /// </summary>
+        public string DiaryName { get { return this.diaryName; } set { this.diaryName = value; } }
+
+        /// <summary>
+        /// Время создания
+        /// </summary>
+        public DateTime CreatedAt { get { return this.createdAt; } set { this.createdAt = value; } }
+
+        /// <summary>
+        /// Время последнего редактирования
+        /// </summary>
+        public DateTime UpdatedAt { get { return this.updatedAt; } set { this.updatedAt = value; } }
+
+        #endregion
+
+        #region Поля
+
+        /// <summary>
+        /// Поле "Название"
+        /// </summary>
+        private string noteName;
+
+        /// <summary>
+        /// Поле "Текст заметки"
+        /// </summary>
+        private string noteText;
+
+        /// <summary>
+        /// Поле "ID ежедневника"
+        /// </summary>
+        private string diaryName;
+
+        /// <summary>
+        /// Поле "Время создания"
+        /// </summary>
+        private DateTime createdAt;
+
+        /// <summary>
+        /// Поле "Время последнего редактирования"
+        /// </summary>
+        private DateTime updatedAt;
+
+        #endregion
+    }
+}
