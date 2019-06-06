c8b9a43120a937899c53bfb5eb4d90d603eaf7ef
NikulinIlya <ilia.nikulin@gmail.com>
2019-06-06 11:05:04 +0300

    make structure DiariesRepository

---
+++ HW6/DiariesRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_6
+{
+    /// <summary>
+    /// Структура для работы с ежедневниками
+    /// </summary>
+    struct DiariesRepository
+    {
+        public DiariesRepository(string PathToDiaries)
+        {
+            this.pathToDiaries = PathToDiaries;
+        }
+
+        //private Diary[] diaries;
+
+        /// <summary>
+        /// Поле "Путь к хранению ежедневников"
+        /// </summary>
+        private string pathToDiaries;
+
+        /// <summary>
+        /// Представляет путь к хранению ежедневников.
+        /// Разрешено работать в пределах одной директории.
+        /// </summary>
+        public string PathToDiaries { get { return this.pathToDiaries; } set { this.pathToDiaries = value; } }
+
+        /// <summary>
+        /// Метод для создания нового ежедневника.
+        /// </summary>
+        /// <param name="name">Название ежедневника</param>
+        public bool CreateDiary(string name)
+        {
+            if (CheckValidName(name, PathToDiaries))
+            {
+                Diary newDiary = new Diary(name, DateTime.Now, DateTime.Now);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void ReadDiary()
+        {
+
+        }
+
+        public void EditDiary()
+        {
+
+        }
+
+        public void DeleteDiary()
+        {
+
+        }
+
+        private static bool CheckValidName(string name, string pathToDiaries)
+        {
+
+            return true;
+        }
+    }
+}
